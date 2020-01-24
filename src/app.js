fd84176726abdbf4dbef73f94d59f38ec48a7d78
Den <denisanatoliyovich@gmail.com>
2020-01-24 20:28:41 +0200

    Now side picking isn't availaible after we start a game. When the game starts only one side is active, which is noticeable . When we press start new game all the scores reset and buttons at the bottom become activated. Pressing holdPoints we can move our round points to the total and pass the turn to our opponent. Pressing on roll rice plays animation of a rolling dice and throws a random value. Animation is implemented with setInterval function In CSS:  added pointing cursor on side picking labels In HTML: buttons are disabled by default

--- src/app.js
+++ src/app.js
@@ -9,6 +9,14 @@ GAME RULES:
 
 */
 
+const dice1 = "./img/dice-1.png";
+const dice2 = "./img/dice-2.png";
+const dice3 = "./img/dice-3.png";
+const dice4 = "./img/dice-4.png";
+const dice5 = "./img/dice-5.png";
+const dice6 = "./img/dice-6.png";
+const dices = [dice1, dice2, dice3, dice4, dice5, dice6];
+
 const playerOneName_p = document.getElementById("player-1-name");
 const playerTwoName_p = document.getElementById("player-2-name");
 
@@ -26,8 +34,17 @@ const newGame_but = document.getElementById("new-game");
 const rollDice_but = document.getElementById("roll-dice");
 const hold_but = document.getElementById("hold");
 
+const pickOpponent_div = document.querySelector(".checkboxes");
 const pickFriend_radio = document.getElementById("pick-friend");
 const pickAI_radio = document.getElementById("pick-ai");
+
+let gameIsInProgress = false;
+let playerOneTurn = false;
+
+let playerOneRoundScore = 0;
+let playerTwoRoundScore = 0;
+let playerOneTotalScore = 0;
+let playerTwoTotalScore = 0;
 
 pickAI_radio.addEventListener("click", () => {
     playerTwoName_p.innerHTML = "AI";
@@ -36,3 +53,125 @@ pickAI_radio.addEventListener("click", () => {
 pickFriend_radio.addEventListener("click", () => {
     playerTwoName_p.innerHTML = "PLAYER 2";
 });
+
+const onOffSidePicking = () => {
+    pickFriend_radio.disabled = gameIsInProgress;
+    pickAI_radio.disabled = gameIsInProgress;
+    // pickOpponent_div.classList = "checkboxes absolute disabled";
+    pickOpponent_div.style.opacity = gameIsInProgress ? 0.5 : 1;
+};
+
+const resetScores = () => {
+    playerOneRoundScore_div.innerHTML = 0;
+    playerTwoRoundScore_div.innerHTML = 0;
+    playerOneTotalScore_div.innerHTML = 0;
+    playerTwoTotalScore_div.innerHTML = 0;
+};
+
+const activeGameButtons = () => {
+    hold_but.disabled = !hold_but.disabled;
+    rollDice_but.disabled = !rollDice_but.disabled;
+};
+
+const startNewGame = () => {
+    gameIsInProgress = !gameIsInProgress;
+    onOffSidePicking();
+    resetScores();
+    activeGameButtons();
+    playerOnePanel_div.classList = "player-1-panel active";
+    playerTwoPanel_div.classList = "player-2-panel";
+    playerOneTurn = true;
+};
+
+const passTurn = () => {
+    playerOneTurn = !playerOneTurn;
+
+    playerOnePanel_div.classList = `player-1-panel ${
+        playerOneTurn ? " active" : ""
+    }`;
+    playerTwoPanel_div.classList = `player-2-panel ${
+        !playerOneTurn ? " active" : ""
+    }`;
+};
+
+const holdPoints = () => {
+    if (playerOneTurn) {
+        playerOneTotalScore += playerOneRoundScore;
+    } else {
+        playerTwoTotalScore += playerTwoRoundScore;
+    }
+
+    passTurn();
+    rollDiceAnimations();
+};
+
+const addRoundPoints = (roundPoints) => {
+    if(playerOneTurn){
+        playerOneRoundScore += roundPoints
+    }else{
+        playerTwoRoundScore += roundPoints
+    }
+} 
+
+const resetRoundPoints = () => {
+    if(playerOneTurn){
+        playerOneRoundScore = 0
+        playerOneRoundScore_div
+    }else{
+        playerTwoRoundScore = 0
+    }
+}
+
+const generateRandomDice = () => {
+    const randomNumber = Math.floor(Math.random() * 6);
+    dice_img.src = dices[randomNumber];
+    return randomNumber + 1;
+};
+
+let diceCounter = 0;
+
+const rollDiceAnimations = () => {
+    dice_img.src = dices[diceCounter];
+
+    diceCounter = diceCounter === 5 ? 0 : ++diceCounter;
+    console.log(diceCounter);
+    // window.requestAnimationFrame
+};
+
+const rollDice = () => {
+    const timeout = 100;
+    const start = Date.now();
+
+    const intervalID = setInterval(() => {
+        if ((Date.now() - start) / 1000 < (timeout * 6) / 1000) {
+            rollDiceAnimations();
+        } else {
+            clearInterval(intervalID);
+            const randomDice = generateRandomDice();
+
+            if(randomDice === 1){
+                resetRoundPoints()
+            }else{
+                addRoundPoints(randomDice)
+            }
+        }
+    }, timeout);
+
+    // const timerId = setTimeout(function run() {
+    //     rollDiceAnimations();
+
+    //     if ((Date.now() - start) / 1000 < (timeout * 6) / 1000) {
+    //         // clearInterval(intervalID);
+    //         console.log((Date.now() - start) / 1000 + "      Time");
+    //         setTimeout(run, timeout);
+    //     } else {
+    //         clearTimeout(timerId);
+    //     }
+    // }, timeout);
+};
+
+newGame_but.addEventListener("click", startNewGame);
+
+hold_but.addEventListener("click", holdPoints);
+
+rollDice_but.addEventListener("click", rollDice);
